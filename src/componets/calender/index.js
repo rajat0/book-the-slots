9294f6f0a0a2710645eae8353439fe58f4c8bf45
rajat <rajatchhapre14@gmail.com>
2024-08-22 11:54:45 +0530

    consmatic changes

--- src/componets/calender/index.js
+++ src/componets/calender/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import moment from "moment";
 import { AgGridReact } from "ag-grid-react";
@@ -6,7 +6,6 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 
 const Calender = ({ calendarId }) => {
-  const [events, setEvents] = useState([]);
   const [error, setError] = useState(null);
   const [rowData, setRowData] = useState([])
   const [colDefs, setColDefs] = useState([
@@ -14,59 +13,60 @@ const Calender = ({ calendarId }) => {
     { field: 'eventTime' },
     { field: 'timeZone' }
   ])
-  useEffect(() => {
-    const fetchEvents = async () => {
-
-      try {
-        const response = await axios.get(
-          `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
-          {
-            params: {
-              key: 'AIzaSyCPeQB3rkP6f7er4txaXJ4ZlbjO8nNSM8c',  // Replace with your API key
-              maxResults: 10,       // Number of events to retrieve
-              orderBy: 'startTime',
-              singleEvents: true,
-              timeMin: new Date().toISOString(),  // Start time (current date and time)
-            },
-          }
-        );
-        // console.log(response.data)
-        setEvents(response.data.items);
-
-        settingRowData();
-
-      } catch (err) {
-        setError(err.message);
-      }
-    };
-    fetchEvents();
-  }, [calendarId])
-
-  console.log(rowData, 'rowData')
 
 
-
-  const settingRowData = async () => {
-    await setRowData(events && events.map(item => {
-      return (
+  const fetchEvents = useCallback(async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
         {
-          'event': item.summary,
-          'eventTime': moment(item.start.dateTime).calendar(),
-          'timeZone': item.start.timeZone
+          params: {
+            key: 'AIzaSyCPeQB3rkP6f7er4txaXJ4ZlbjO8nNSM8c',  // Replace with your API key
+            maxResults: 10,       // Number of events to retrieve
+            orderBy: 'startTime',
+            singleEvents: true,
+            timeMin: new Date().toISOString(),  // Start time (current date and time)
+          },
         }
-      )
-    }))
-  }
+      );
+      setRowData(response.data.items.map(item => {
+        return (
+          {
+            'event': item.summary,
+            'eventTime': moment(item.start.dateTime).calendar(),
+            'timeZone': item.start.timeZone
+          }
 
+        )
+      }))
+    } catch (err) {
+      setError(err.message);
+    }
+
+  }, [calendarId])
 
 
   return (
-    <div>
-      <div className="ag-theme-quartz" style={{ height: '300px', width: '50%' }}>
-        <AgGridReact
-          rowData={rowData}
-          columnDefs={colDefs}
-        />
+    <div className="">
+      <div className="flex-row  p-4">
+        <div className=" flex justify-center bg-gray-300 items-center p-2 rounded-md">
+          <div onClick={fetchEvents} className="border-2-solid-black rounded-md bac" >Click To Fetch Data</div>
+        </div>
+
+        {
+          rowData.length > 0 &&
+          <div className="flex justify-center bg-gray-300 items-center p-4 mt-2 rounded-md">
+            <div className="ag-theme-quartz" style={{ height: '300px', width: '50%' }}>
+              <AgGridReact
+                rowData={rowData.length && rowData}
+                columnDefs={colDefs}
+
+              />
+            </div>
+          </div>
+        }
+
       </div>
     </div>
   );
